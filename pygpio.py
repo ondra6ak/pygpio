dc4ee24df26d3c48c3958adf4749d8f19bcfebc3
ondra6ak <ondra6ak@gmail.com>
2015-06-17 00:22:23 +0200

    Initial commit

---
+++ pygpio.py
@@ -0,0 +1,26 @@
+class Gpio:
+    def __init__(self, gpio, direction="out"):
+        self.gpio = gpio
+        self.direction = direction
+        self.path = "/sys/class/gpio{}/".fromat(self.gpio)
+        
+        open("/sys/class/gpio/export", "w").write(str(self.gpio))
+        open(self.path + "direction", "w").write(self.direction)
+
+    def set(self, value):
+        self.value = value
+        open(self.path + "value", "w").write(str(value))
+
+    def get(self):
+        return open(self.path + "value", "r").read()
+
+    def toggle(self):
+        if self.value == 0:
+            open(self.path + "value", "w").write("1")
+            self.value = 1
+        else:
+            open(self.path + "value", "w").write("0")
+            self.value = 0
+    
+    def __del__():
+        open("/sys/class/unexport", "w").write(str(self.gpio))
