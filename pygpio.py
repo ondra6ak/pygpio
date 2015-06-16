414e8d88cfc373f4027f3a3289525719b437c666
ondra6ak <ondra6ak@gmail.com>
2015-06-17 00:41:32 +0200

    toggle method now implemented properly

--- pygpio.py
+++ pygpio.py
@@ -3,7 +3,6 @@ class Gpio:
         self.gpio = gpio
         self.direction = direction
         self.path = "/sys/class/gpio/gpio{}/".format(self.gpio)
-        self.value = 0
 
         open("/sys/class/gpio/export", "w").write(str(self.gpio))
         open(self.path + "direction", "w").write(self.direction)
@@ -16,12 +15,10 @@ class Gpio:
         return int(open(self.path + "value", "r").read().replace("\n", ""))
 
     def toggle(self):
-        if self.value == 0:
+        if self.get() == 0:
             open(self.path + "value", "w").write("1")
-            self.value = 1
         else:
             open(self.path + "value", "w").write("0")
-            self.value = 0
     
     def __del__(self):
         open("/sys/class/gpio/unexport", "w").write(str(self.gpio))
